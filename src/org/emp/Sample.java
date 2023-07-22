b2705347c095d2273aaba608c5f4e18f6b7fe587
Raajesh123 <rajesh.eie94@gmail.com>
2023-07-22 14:16:48 +0530

    Commit message

---
+++ src/org/emp/Sample.java
@@ -0,0 +1,29 @@
+package org.emp;
+
+public class Sample {
+	
+		
+		private void m1() {
+			System.out.println("asik");
+				}
+		
+	private void m2() {
+	System.out.println("rejesh");
+		}
+		
+		private void m3() {
+			System.out.println("prabhu");
+				}
+		
+		private void m4() {
+			System.out.println("vimal");
+				}
+	public static void main(String[] args) {
+		System.out.println("done");
+		
+		
+		
+	}
+	}
+
+
